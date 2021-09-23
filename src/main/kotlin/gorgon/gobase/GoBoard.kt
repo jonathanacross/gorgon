fd4dd55ede97416bc3f40161de5764ebe329b58e
Jonathan Cross <jonathan.a.cross@gmail.com>
2021-09-23 00:03:41 -0700

    adding more functions to GoBoard

--- src/main/kotlin/gorgon/gobase/GoBoard.kt
+++ src/main/kotlin/gorgon/gobase/GoBoard.kt
@@ -1,5 +1,7 @@
 package gorgon.gobase
 
+import java.util.*
+
 data class GoBoard(
     val size: Int,
     val data: IntArray,
@@ -133,5 +135,188 @@ data class GoBoard(
             val data = getEmptyBoardDataForSize(size)
             return GoBoard(size, data, noKo)
         }
+    }
+
+    fun isLegalMove(squareType: Int, idx: Int): Boolean {
+        // square must be empty (duh!)
+        if (data[idx] != SquareType.Empty) {
+            return false
+        }
+
+        if (idx == ko) {
+            // Only retake a ko if it's a snapback.
+            return getCapturedStoneList(squareType, idx).size > 1
+        }
+
+        // If there are liberties adjacent to current move, then for sure this
+        // isn't suicide.
+        if (data[Location.left(idx)] == SquareType.Empty ||
+            data[Location.up(idx)] == SquareType.Empty ||
+            data[Location.right(idx)] == SquareType.Empty ||
+            data[Location.down(idx)] == SquareType.Empty
+        ) {
+            return true
+        }
+
+        // Suicide moves are legal only if we make a capture.
+        if (isPotentialSuicide(squareType, idx)) {
+            return getCapturedStoneList(squareType, idx).isNotEmpty()
+        } else {
+            return true
+        }
+    }
+
+    private fun isPotentialSuicide(value: Int, idx: Int): Boolean {
+        val supposePlay = this.set(value, idx)
+        return supposePlay.isGroupSurrounded(idx)
+    }
+
+    fun legalMoves(player: Player): List<Int> {
+        val squareType = SquareType.playerToSquareType(player)
+        return boardSquares().filter { i -> isLegalMove(squareType, i) }.toList()
+    }
+
+    // TODO: rename to getCapturedStones?
+    fun getCapturedStoneList(value: Int, idx: Int): List<Int> {
+        val opp = SquareType.opposite(value)
+        if ((data[Location.up(idx)] != opp) &&
+            (data[Location.down(idx)] != opp) &&
+            (data[Location.left(idx)] != opp) &&
+            (data[Location.right(idx)] != opp)
+        ) {
+            return listOf()
+        }
+
+        require(data[idx] == SquareType.Empty)
+
+        // temporarily place the stone
+        data[idx] = value
+
+        val captured = ArrayList<Int>()
+        for (nbr in neighbors(idx)) {
+            if (data[nbr] == opp) {
+                if (isGroupSurrounded(nbr)) {
+                    val group = floodfill(nbr, { x: Int -> x == data[nbr] })
+                    captured.addAll(group)
+                }
+            }
+        }
+
+        // remove the stone
+        data[idx] = SquareType.Empty
+        return captured.toList()
+    }
+
+    private fun neighbors(idx: Int): List<Int> {
+        val allnbrs =
+            listOf(Location.left(idx), Location.right(idx), Location.up(idx), Location.down(idx))
+        return allnbrs.filter { i -> data[i] != SquareType.OffBoard }
+    }
+
+    // Tromp-Taylor scoring. The Tromp-Taylor ruleset requires the game to be
+    // played out until all dead stones are removed, then uses area (Chinese)
+    // scoring.
+    fun score(): Pair<Int, Int> {
+        val whiteSquares = boardSquares().filter { i -> data[i] == SquareType.White }
+        val blackSquares = boardSquares().filter { i -> data[i] == SquareType.Black }
+
+        val reachableWhite = floodfillmulti(
+            whiteSquares,
+            { x: Int -> (x == SquareType.Empty || x == SquareType.White) }).toSet()
+        val reachableBlack = floodfillmulti(
+            blackSquares,
+            { x: Int -> (x == SquareType.Empty || x == SquareType.Black) }).toSet()
+
+        val whiteOnly = reachableWhite subtract reachableBlack
+        val blackOnly = reachableBlack subtract reachableWhite
+
+        return Pair(blackOnly.size, whiteOnly.size)
+    }
+
+    // flood fill an area that follows a given predicate, starting at the list
+    // of squares startIdx.
+    // TODO: more efficient algorithm, possibly http://will.thimbleby.net/scanline-flood-fill/
+    private fun floodfillmulti(startIdx: Iterable<Int>, rule: (Int) -> Boolean): List<Int> {
+        val painted = Array(data.size) { false }
+        val paintLocations = ArrayDeque<Int>()
+        val groupList = ArrayList<Int>()
+
+        for (i in startIdx) {
+            paintLocations.push(i)
+            painted[i] = true
+            groupList += i
+        }
+
+        while (!paintLocations.isEmpty()) {
+            val pixel = paintLocations.pop()
+            val nbrs = neighbors(pixel)
+            for (p in nbrs) {
+                if (!painted[p] && rule(data[p])) {
+                    paintLocations.push(p)
+                    groupList += p
+                    painted[p] = true
+                }
+            }
+        }
+
+        return groupList.toList()
+    }
+
+    // flood fill an area that follows a given predicate
+    // TODO: more efficient algorithm, possibly http://will.thimbleby.net/scanline-flood-fill/
+    private fun floodfill(startIdx: Int, rule: (Int) -> Boolean): List<Int> {
+        if (!rule(data[startIdx])) {
+            return listOf()
+        }
+
+        val painted = HashSet<Int>()
+        val paintLocations = ArrayDeque<Int>()
+        val groupList = ArrayList<Int>()
+
+        paintLocations.push(startIdx)
+        painted += startIdx
+        groupList += startIdx
+
+        while (!paintLocations.isEmpty()) {
+            val pixel = paintLocations.pop()
+            val nbrs = neighbors(pixel)
+            for (p in nbrs) {
+                if (!painted.contains(p) && rule(data[p])) {
+                    paintLocations.push(p)
+                    groupList += p
+                    painted += p
+                }
+            }
+        }
+
+        return groupList.toList()
+    }
+
+    private fun isGroupSurrounded(startIdx: Int): Boolean {
+        val sameColor = data[startIdx]
+
+        val painted = HashSet<Int>()
+        val checkLocations = ArrayDeque<Int>()
+
+        checkLocations.push(startIdx)
+        painted += startIdx
+        var foundLiberty = false
+
+        while (!checkLocations.isEmpty() && !foundLiberty) {
+            val pixel = checkLocations.pop()
+            val nbrs = neighbors(pixel)
+            for (p in nbrs) {
+                if (!painted.contains(p)) {
+                    if (data[p] == SquareType.Empty) {
+                        foundLiberty = true
+                    } else if (data[p] == sameColor) {
+                        checkLocations.push(p)
+                        painted += p
+                    }
+                }
+            }
+        }
+
+        return !foundLiberty
     }
 }
